14f39bb2194c6acfa6bd2c8304ed688591d0427f
Kevin Wong <k.wong4@yahoo.com>
2017-05-10 22:11:00 -0400

    Update best_answers.rb

--- best_answers.rb
+++ best_answers.rb
@@ -1,5 +1,11 @@
+###Turn this string…
+#"The quick brown fox jumps over the lazy dog"
+#…into this string…
+#"abcdefghijklmnopqrstuvwxyz"
+
 "The quick brown fox jumps over the lazy dog".delete(" ").downcase.chars.uniq.sort.join
 
+###Egyptian Asterisk Pyramid
 height = ARGV[0].to_i
 output = ""
 height.times do |i|
